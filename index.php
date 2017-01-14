2bf8516a2dccf7c7a86e1651d1505f31e862e977
bewoksatukosong <bewoksatukosong@gmail.com>
2017-01-14 12:18:55 -0500

    update index.php

--- index.php
+++ index.php
@@ -27,23 +27,6 @@
             <a class="navbar-brand" href="index.php">Login Admin</a>
           </div>
               <div class="navbar-collapse collapse">
-                <!--<ul class="nav navbar-nav">
-                  <li class="active"><a href="#">Admin</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#contact">Contact</a></li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li class="divider"></li>
-                      <li class="dropdown-header">Nav header</li>
-                      <li><a href="#">Separated link</a></li>
-                      <li><a href="#">One more separated link</a></li>
-                    </ul>
-                  </li>
-                </ul>-->
                 <ul class="nav navbar-nav navbar-right">
                   <li>
                       <a href="#" class="active"><span class="glyphicon glyphicon-user"></span>
@@ -73,11 +56,10 @@
   }
   $_SESSION['start_time'] = time();
 ?>
-
   <div class="container">
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1>Selamat, Anda Berhasil Login.!</h1>
+        <h1>Selamat, Anda Berhasil Login.!<br/><?php $fullname = $_SESSION['fullname']; echo $fullname; ?></h1>
         <p>
           <a class="btn btn-lg btn-primary" href="logout.php" onclick="return confirm('Apakah anda akan keluar?');">Keluar »</a>
         </p>
